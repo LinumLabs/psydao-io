e0748db893cc9824bca6019c575e2578352048c6
arlindlinum <arlind@linumlabs.com>
2024-04-30 16:18:04 +0200

    feat: improvements for responsive

--- components/windows/blog.tsx
+++ components/windows/blog.tsx
@@ -1,21 +1,24 @@
 import { Window } from "components/window";
 import Iframe from "../iframe";
 import { useWindowManager } from "../window-manager";
+import { useMediaQuery } from "@chakra-ui/react";
 
 export const Blog = () => {
+  const [isLargerThanMd] = useMediaQuery("(min-width: 768px)");
+
   const { dispatch } = useWindowManager();
   const id = "blog";
 
   return (
     <Window
       id={id}
-      height="100%"
+      height="80%"
       maxHeight="640px"
-      minHeight="350px"
+      minHeight={isLargerThanMd ? "500px" : "350px"}
       width="95%"
       maxWidth="655px"
       minWidth="240px"
-      top="40%"
+      top={{ base: "46%", md: "42%" }}
       left={{ base: "50%", lg: "40%" }}
       transform="translate(-50%, -50%)"
       defaultIsOpen
