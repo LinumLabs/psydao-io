99ff669915e4dc031d5c0ee04b8d6c24808df455
arlindlinum <arlind@linumlabs.com>
2024-04-30 12:19:05 +0200

    feat: fix padding on scroll

--- components/terms-and-conditions/TermsAndConditionsContent.tsx
+++ components/terms-and-conditions/TermsAndConditionsContent.tsx
@@ -6,37 +6,38 @@ import TsCsPartThree from "./TsCsPartThree";
 const TermsAndConditionsContent = () => {
   return (
     <Box
-      p={{ base: "8px", md: "16px" }}
-      background={"#FBE7E8"}
-      borderRadius={"16px"}
       overflowY={"auto"}
-      css={{
-        "&::-webkit-scrollbar": {
-          width: "11px"
-        },
-        "&::-webkit-scrollbar-track": {
-          width: "8px",
-          background: "white",
-          borderRadius: "24px",
-          border: "1px solid #F2BEBE"
-        },
-        "&::-webkit-scrollbar-thumb": {
-          background: "#F2BEBE",
-          border: "1.5px solid transparent",
-          backgroundClip: "padding-box",
-          borderRadius: "24px",
-          width: "6px"
-        },
-
-        "@media (min-width: 360px) (min-height: 600px) (max-height: 1000px)": {
-          maxHeight: "320px"
-        }
-      }}
+      background={"#FBE7E8"}
+      p={{ base: "8px", md: "16px" }}
+      borderRadius={"16px"}
     >
       <Flex
+        overflowY={"auto"}
+        css={{
+          "&::-webkit-scrollbar": {
+            width: "11px"
+          },
+          "&::-webkit-scrollbar-track": {
+            width: "8px",
+            background: "white",
+            borderRadius: "24px",
+            border: "1px solid #F2BEBE"
+          },
+          "&::-webkit-scrollbar-thumb": {
+            background: "#F2BEBE",
+            border: "1.5px solid transparent",
+            backgroundClip: "padding-box",
+            borderRadius: "24px",
+            width: "6px"
+          },
+
+          "@media (min-width: 360px) (min-height: 600px) (max-height: 1000px)":
+            {
+              maxHeight: "320px"
+            }
+        }}
         h={"100%"}
         direction={"column"}
-        // maxH={{ base: "300px", md: "260px", lg: "280px" }}
         color={"#591D6D"}
         justifyContent={"flex-start"}
         textAlign={"start"}
