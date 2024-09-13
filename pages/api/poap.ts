bba456bb30427fd7a7af30021293aa967d2135b5
Bevan Christians <gaelicgod@gmail.com>
2024-09-13 14:44:19 +0200

    refactor: check ens name; get extra data

--- pages/api/poap.ts
+++ pages/api/poap.ts
@@ -1,8 +1,63 @@
 import { env } from "@/config/env.mjs";
 import type { NextApiRequest, NextApiResponse } from "next";
-import type { Address } from "viem";
+import { createPublicClient, getAddress, http, isAddress, type Address } from "viem";
+import { mainnet } from 'viem/chains';
 
 const POAP_EVENT_ID = env.POAP_EVENT_ID;
+
+const publicClient = createPublicClient({
+  chain: mainnet,
+  transport: http()
+});
+
+/**
+ * Validates and normalizes an Ethereum address from a query string.
+ * 
+ * @param address - The address or ENS name to validate and normalize.
+ * @returns A Promise that resolves to the normalized Ethereum address (type Address)..
+ * @throws {Error} If the address is not a valid Ethereum address or ENS name.
+ */
+async function getAddressFromQuery(address: string): Promise<Address> {
+  if (isAddress(address)) {
+    return getAddress(address);
+  }
+
+  // check if its an ENS name
+  const resolvedAddress: Address | null = await publicClient.getEnsAddress({ name: address });
+  if (resolvedAddress) {
+    return resolvedAddress;
+  }
+
+  throw new Error("Invalid ethereum address");
+}
+
+/**
+ * Fetches POAP event data for a given event ID.
+ * 
+ * @param eventId - The ID of the POAP event to fetch.
+ * @returns A Promise that resolves to the POAP event data.
+ * @throws {Error} If the API request fails or returns a non-OK status.
+ */
+async function getPoapEvent(eventId: string) {
+  const poapRes = await fetch(
+    `https://api.poap.tech/events/id/${eventId}`,
+    {
+      headers: {
+        accept: "application/json",
+        "X-API-KEY": env.POAP_API_KEY
+      },
+      method: "GET"
+    }
+  );
+
+  if (!poapRes.ok) {
+    console.error('Failed to fetch POAP event', poapRes.status, poapRes.statusText);
+    throw new Error("Failed to fetch POAP event");
+  }
+
+  const eventData = await poapRes.json() as unknown as Record<string, PropertyKey>;
+  return eventData;
+}
 
 export default async function handler(
   req: NextApiRequest,
@@ -10,10 +65,20 @@ export default async function handler(
 ) {
   if (req.method === "GET") {
     try {
-      const { address } = req.query as { address: Address };
+      const { address } = req.query as { address: string };
+      const normalizedAddress = await getAddressFromQuery(address);
+      const poapEvent = await getPoapEvent(POAP_EVENT_ID);
+
+      console.log("data: ", {
+        address,
+        POAP_EVENT_ID,
+        POAP_API_KEY: env.POAP_API_KEY,
+        normalizedAddress,
+        poapEvent
+      });
       // TODO: Get type for POAP response body
       const poapRes = await fetch(
-        `https://api.poap.tech/actions/scan/${address}/${POAP_EVENT_ID}`,
+        `https://api.poap.tech/actions/scan/${normalizedAddress}/${POAP_EVENT_ID}`,
         {
           headers: {
             accept: "application/json",
