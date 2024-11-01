6bcbea3218464bb70eb8abad51fc768d5151b8a7
Ester <ester@linumlabs.com>
2024-11-01 09:25:45 +0200

    add invalid deadline error message

--- hooks/useCustomToasts.ts
+++ hooks/useCustomToasts.ts
@@ -57,7 +57,8 @@ const ERROR_MESSAGES = {
   "Invalid Proof": "Invalid proof submitted.",
   "Not Enough Psy In Contract": "Not enough PSY in contract to fulfill claim.",
   "All Psy Claimed": "All PSY has been claimed.",
-  "Claim Period Ended": "The claim period has already ended."
+  "Claim Period Ended": "The claim period has already ended.",
+  "Invalid Deadline": "The selected deadline is too close to the current date."
 };
 
 export const customErrorMessage = (message: string): string => {
